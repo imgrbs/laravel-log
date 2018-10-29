39d132b434214ac630ae9b85c45147b62590eca9
Keerati <keerati.jear@gmail.com>
2018-10-29 13:24:23 +0700

    [UPDATE] Update docs comment and refactor log

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,11 +7,23 @@ use Illuminate\Support\Facades\Log;
 
 class HelloController extends Controller
 {
+    /**
+     * @api {get} /hello
+     * @apiName Get Hello
+     * @apiSuccessExample {json} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36",
+     *       "ip": "127.0.0.1"
+     *     }
+     */
     public function getHello(Request $request) {
-        Log::info('IP:' .  $request->ip() . ', User-Agent:' . $request->header('User-Agent'));
+        $userAgent = $request->header('User-Agent');
+        $ip = $request->ip();
+        Log::info('IP:' . $ip . ', User-Agent:' . $userAgent);
         return response()->json([
-            'header' => $request->header(),
-            'ip' => $request->ip(),
+            'user-agent' => $userAgent,
+            'ip' => $ip,
         ]);
     }
 }
