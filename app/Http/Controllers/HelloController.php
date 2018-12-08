3f67a5cbb483d60e68459eefc4dc741ae1fb6d96
Keerati <keerati.jear@gmail.com>
2018-12-08 19:01:50 +0700

    update some proof

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
+use BracketGenerator\Bracket;
+
 class HelloController extends Controller
 {
     /**
@@ -20,7 +22,6 @@ class HelloController extends Controller
     public function getHello(Request $request) {
         $userAgent = $request->header('User-Agent');
         $ip = $request->ip();
-        // Log::info('IP:' . $ip . ', User-Agent:' . $userAgent);
         $logData= [
             "ip" => $ip,
             "User-Agent" => $userAgent,
@@ -28,5 +29,47 @@ class HelloController extends Controller
         ];
         Log::info(json_encode($logData));
         return response()->json($logData);
+    }
+
+    // ========================
+    // Example Code
+    // ========================
+    // $teams = [
+    //     [ 'Participant 1', 0 ],
+    //     [ 'Participant 2', 0 ],
+    //     [ 'Participant 3', 0 ],
+    //     [ 'Participant 4', 0 ],
+    //     [ 'Participant 5', 0 ],
+    //     [ 'Participant 6', 0 ],
+    // ];
+    // $brackets = Bracket::create(6);
+    // $brackets->fillByParticipantList($teams);
+    // return response()->json($brackets->getTree());
+    // ========================
+
+    public function getBrackets(Request $request) {
+        $teams = [
+            [ 'Participant 1' ],
+            [ 'Participant 2' ],
+            [ 'Participant 3' ],
+            [ 'Participant 4' ],
+            [ 'Participant 5' ],
+            [ 'Participant 6' ],
+        ];
+        shuffle($teams);
+
+        $teamCount = count($teams);
+        $matchs = array_fill(0, $teamCount / 2, []);
+        $currentMatch = 0;
+        for ($index = 0; $index < $teamCount; $index ++) {
+            if (count($matchs[$currentMatch]) < 2) {
+                $team = array_pop($teams[$index]);
+                array_push($matchs[$currentMatch], $team);
+            }
+            if (count($matchs[$currentMatch]) == 2) {
+                $currentMatch++;
+            }
+        }
+        return response()->json($matchs);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/hello', 'HelloController@getHello');
+Route::get('/brackets', 'HelloController@getBrackets');
